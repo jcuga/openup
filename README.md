82d8bf75116f8ab7b7a580318698c8657ccbd97e
jcuga <jcuga@users.noreply.github.com>
2016-08-29 21:25:53 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # openup
 Utility that opens port forwards via upnp.  Written in golang.
+
+## Usage
+```
+$ ./openup -h
+Usage:
+  -close
+        Close (as opposed to open) the given port.
+  -ip
+        Display external IP address and exit.
+  -port int
+        Port to open/close (default -1)
+  -udp
+        Use UDP (instead of TCP) when opening/closing port forward.
+```
+
+###To discover your external IP:
+```
+$ ./openup -ip
+Your external IP is: 123.123.123.123
+```
+
+###To open up a port forward:
+```
+$ ./openup -port 9978
+Opening port forward for TCP port 9978
+
+$ ./openup -port 9979 -udp
+Opening port forward for UDP port 9979
+```
+
+###To close a port forward:
+```
+$ ./openup -close -port 5233 -udp
+Closing port forward for UDP port 5233
+```
+
+TODO: how long will port forward last if you don't explicitly close it?
---
+++ openup.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jcuga/go-upnp"
+	"os"
+)
+
+func main() {
+	displayExternalIpPtr := flag.Bool("ip", false, "Display external IP address and exit.")
+	useUdpPtr := flag.Bool("udp", false, "Use UDP (instead of TCP) when opening/closing port forward.")
+	doClosePtr := flag.Bool("close", false, "Close (as opposed to open) the given port.")
+	portPtr := flag.Int("port", -1, "Port to open/close")
+	flag.Parse()
+	if *displayExternalIpPtr {
+		// connect to router
+		d, dErr := upnp.Discover()
+		if dErr != nil {
+			fmt.Printf("Error discovering router: %v", dErr)
+			os.Exit(1)
+		}
+		// discover external IP
+		ip, _ := d.ExternalIP()
+		fmt.Println("Your external IP is:", ip)
+		return
+	}
+	if *portPtr < 0 {
+		fmt.Printf("Invalid port number, must be > 0.  Got: %d", *portPtr)
+		os.Exit(1)
+	}
+	d, dErr := upnp.Discover()
+	if dErr != nil {
+		fmt.Printf("Error discovering router: %v", dErr)
+		os.Exit(1)
+	}
+	var proto string
+	if *useUdpPtr {
+		proto = "UDP"
+	} else {
+		proto = "TCP"
+	}
+	// Open/Close the given port/protocol
+	if *doClosePtr {
+		fmt.Printf("Closing port forward for %s port %d\n", proto, *portPtr)
+		cErr := d.Clear(uint16(*portPtr), proto)
+		if cErr != nil {
+			fmt.Printf("Error closing port forward: %v", cErr)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("Opening port forward for %s port %d\n", proto, *portPtr)
+		fErr := d.Forward(uint16(*portPtr), "Requested by openup util.", proto)
+		if fErr != nil {
+			fmt.Printf("Error closing port forward: %v", fErr)
+			os.Exit(1)
+		}
+	}
+}
